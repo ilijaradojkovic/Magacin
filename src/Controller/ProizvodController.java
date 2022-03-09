246f7520c78b7677a348aedc3b435b4b0852523d
ilijaradojkovic <radojkovicika@gmail.com>
2022-03-09 18:15:23 +0300

    Uradjena implementacija metoda  osnovnih operacija

--- src/Controller/ProizvodController.java
+++ src/Controller/ProizvodController.java
@@ -1,25 +1,36 @@
 package Controller;
 
+import java.util.LinkedList;
+import java.util.List;
+
 import Artikli.BaseProizvod;
 import Interfejs.IOperacije;
 
 public class ProizvodController implements IOperacije{
 
+	List<BaseProizvod> proizvodi;
+	
+	public ProizvodController() {
+		proizvodi=new LinkedList<BaseProizvod>();
+	}
 	@Override
 	public void AddProizvod(BaseProizvod proizvod) {
-		// TODO Auto-generated method stub
 		
+		proizvodi.add(proizvod);
 	}
 
 	@Override
 	public void RemoveProizvod(BaseProizvod proizvod) {
-		// TODO Auto-generated method stub
+		proizvodi.remove(proizvod);
 		
 	}
 
 	@Override
 	public BaseProizvod FindProizvode(int sifra) {
-		// TODO Auto-generated method stub
+		
+		for (BaseProizvod baseProizvod : proizvodi) {
+			if(baseProizvod.getSifra()==sifra) return baseProizvod;
+		}
 		return null;
 	}
 
