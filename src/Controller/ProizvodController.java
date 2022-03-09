5b5f39167afb2c678cd4d14aa798e3484628e23d
ilijaradojkovic <radojkovicika@gmail.com>
2022-03-09 18:08:32 +0300

    Napravljena klasa ProizvodController koja implementuje IOperacije

---
+++ src/Controller/ProizvodController.java
@@ -0,0 +1,26 @@
+package Controller;
+
+import Artikli.BaseProizvod;
+import Interfejs.IOperacije;
+
+public class ProizvodController implements IOperacije{
+
+	@Override
+	public void AddProizvod(BaseProizvod proizvod) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void RemoveProizvod(BaseProizvod proizvod) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public BaseProizvod FindProizvode(int sifra) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
